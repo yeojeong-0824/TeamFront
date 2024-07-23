b22136f680241b85d9144807056b6051b5ad588e
jun <znznun@gmail.com>
2024-07-23 21:54:12 +0900

    app폴더 삭제, header에 로그인/회원가입 Link 연결

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 const Header = (): JSX.Element => {
   return (
-    <header className="flex justify-between fixed top-0 z-50 w-full py-5 px-1 bg-white border-b shadow-md">
+    <header className="flex justify-between w-full py-5 px-1 bg-white border-b shadow-md">
       <Link href={'/'}>
         <h1 className="text-2xl font-bold text-blue-500">Seoul Community</h1>
       </Link>
       <div className="flex gap-10 items-center">
         <div className="flex gap-2">
-          <button className="p-2 rounded-xl bg-blue-500 text-white">로그인</button>
-          <button className="p-2 rounded-xl bg-blue-500 text-white">회원가입</button>
+          <Link href={'/login_ui'} className="p-2 rounded-xl bg-blue-500 text-white">로그인</Link>
+          <Link href={'/sign_up'} className="p-2 rounded-xl bg-blue-500 text-white">회원가입</Link>
         </div>
       </div>
     </header>

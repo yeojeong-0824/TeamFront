cc7aa092f2e40d8aa4497257654a0c40a4961e6d
mini <155148431+mini0192@users.noreply.github.com>
2024-08-22 15:33:35 +0900

    개별 게시글 조회 시 해당 유저의 별점 등록 여부 확인 오류 해결

--- src/api/postCall.ts
+++ src/api/postCall.ts
@@ -4,6 +4,8 @@ const postCall = async (id: number) => {
   const response = await axios(`${process.env.NEXT_PUBLIC_API_URL}/board/${id}`, {
     headers: { 
       'Content-Type': 'application/json',
+      // 해당 부분 수정했습니다
+      Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
     }
   });
   return response.data;

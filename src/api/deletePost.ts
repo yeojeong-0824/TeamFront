59b6f3ff4afc32de75126482e122603c56048351
jun <znznun@gmail.com>
2024-10-11 18:09:34 +0900

    기본 env파일 삭제

--- src/api/deletePost.ts
+++ src/api/deletePost.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const deletePost = async (id: number)=> {
+const deletePost = async (id: number) => {
 const response = await axios.delete(`${process.env.NEXT_PUBLIC_API_URL}/boards/authed/${id}`, {
     headers: {
       "Content-Type": "application/json",
@@ -8,6 +8,6 @@ const response = await axios.delete(`${process.env.NEXT_PUBLIC_API_URL}/boards/a
     }
   });
   return response.data;
-}
+};
 
 export default deletePost;

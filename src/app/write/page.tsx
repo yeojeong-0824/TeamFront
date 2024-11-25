699d0593de34c8066cbe3819d889eb04b23cd262
jun <znznun@gmail.com>
2024-11-26 02:39:49 +0900

    글작성 버튼에 로딩처리/댓글 작성 버튼에 로딩처리

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import useWritePost from "@/hooks/useWritePost";
 import { WriteUpdateType } from "@/types/board";
@@ -8,19 +8,20 @@ import PlaceSearch from "../components/PlaceSearch";
 import { useRouter } from "next/navigation";
 import LoadingSpinner from "../components/Loading";
 import ErrorShow from "../components/Error";
-import 'react-quill/dist/quill.snow.css';
+import "react-quill/dist/quill.snow.css";
 import QuillEditor from "../components/Quill";
 import Swal from "sweetalert2";
 import { SetLocalData } from "@/types/write";
+import { Button } from "@nextui-org/react";
 
 const Write = () => {
   const { register, handleSubmit } = useForm<WriteUpdateType>();
   const writeMutation = useWritePost();
-  const [location, setLocation] = useState<string>('');
-  const [formattedAddress, setFormattedAddress] = useState<string>('');
+  const [location, setLocation] = useState<string>("");
+  const [formattedAddress, setFormattedAddress] = useState<string>("");
   const [latitude, setLatitude] = useState<number>(0);
   const [longitude, setLongitude] = useState<number>(0);
-  const [html, setHtml] = useState<string>('');
+  const [html, setHtml] = useState<string>("");
   const router = useRouter();
 
   const setLocalData: SetLocalData = {
@@ -31,21 +32,21 @@ const Write = () => {
   };
 
   const onSubmitForm = (title: WriteUpdateType) => {
-    if (title.title === '' || html === '') {
+    if (title.title === "" || html === "") {
       Swal.fire({
-        icon: 'error',
-        title: '제목과 본문을 입력해주세요.',
+        icon: "error",
+        title: "제목과 본문을 입력해주세요.",
       });
       return;
-    };
+    }
     const locationData = {
       title: title.title,
       body: html,
       locationName: location,
       formattedAddress,
       latitude,
-      longitude
-    }
+      longitude,
+    };
     writeMutation.mutate(locationData);
   };
 
@@ -54,47 +55,68 @@ const Write = () => {
   return (
     <>
       <div className="min-h-[1100px] sm:my-12 p-2 text-gray-900">
-        <LoadingSpinner size={15} mt={40} isLoading={writeMutation.isPending} />
-        {writeMutation.isError && <ErrorShow error={writeMutation.error.message} />}
-        <form className="flex flex-col max-w-[800px] gap-8 mx-auto mt-24 p-3" onSubmit={handleSubmit(onSubmitForm)}>
+        {writeMutation.isError && (
+          <ErrorShow error={writeMutation.error.message} />
+        )}
+        <form
+          className="flex flex-col max-w-[800px] gap-8 mx-auto mt-24 p-3"
+          onSubmit={handleSubmit(onSubmitForm)}
+        >
           <div className="flex flex-col gap-2">
             <h1 className="text-xl sm:text-3xl text-gray-700 leading-10">
               당신의 <span className="text-[#3D6592]">여정</span>을 기록해보세요
             </h1>
             <p className="text-xs sm:text-medium ml-1 text-gray-400">
-              <span className="text-[#3D6592]">여정</span>과 함께 소중한 여행의 기록들을 나눠보는건 어떨까요?
+              <span className="text-[#3D6592]">여정</span>과 함께 소중한 여행의
+              기록들을 나눠보는건 어떨까요?
             </p>
           </div>
 
           <div className="flex flex-col gap-2">
-            <label htmlFor="local-search"
-            className="text-sm sm:text-medium">지역</label>
+            <label htmlFor="local-search" className="text-sm sm:text-medium">
+              지역
+            </label>
             <PlaceSearch setLocalData={setLocalData} />
           </div>
 
           <div className="flex flex-col gap-2">
-            <label htmlFor="title"
-            className="text-sm sm:text-medium">제목</label>
-            <input type="text" placeholder="제목을 입력해주세요." className="p-1.5 sm:p-2 border rounded-md text-sm md:text-base" {...register('title')} />
+            <label htmlFor="title" className="text-sm sm:text-medium">
+              제목
+            </label>
+            <input
+              type="text"
+              placeholder="제목을 입력해주세요."
+              className="p-1.5 sm:p-2 border rounded-md text-sm md:text-base"
+              {...register("title")}
+            />
           </div>
 
           <div className="flex flex-col gap-2">
-            <label htmlFor="body"
-            className="text-sm sm:text-medium">본문</label>
+            <label htmlFor="body" className="text-sm sm:text-medium">
+              본문
+            </label>
             <QuillEditor html={html} setHtml={setHtml} />
           </div>
           <div className="flex justify-end gap-3">
-            <button className='p-1 px-3 sm:p-2 sm:px-6 border text-gray-900 hover:bg-gray-100 rounded-lg text-sm sm:text-base' type="button" onClick={handleCancel}>
+            <button
+              className="p-1 px-3 sm:p-2 sm:px-6 border text-gray-900 hover:bg-gray-100 rounded-lg text-sm sm:text-base"
+              type="button"
+              onClick={handleCancel}
+            >
               취소
             </button>
-            <button className='p-1 px-3 sm:p-2 sm:px-6 text-white bg-[#6EB4FB] hover:bg-blue-500 rounded-lg text-sm sm:text-base'>
+            <Button
+              className="p-1 px-3 sm:p-2 sm:px-6 text-white bg-[#6EB4FB] hover:bg-blue-500 rounded-lg text-sm sm:text-base"
+              type="submit"
+              isLoading={writeMutation.isPending}
+            >
               등록
-            </button>
+            </Button>
           </div>
         </form>
       </div>
     </>
-  )
+  );
 };
 
 export default Write;

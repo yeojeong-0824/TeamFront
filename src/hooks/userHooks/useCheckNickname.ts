d4e5beb8015a25eb32c9f9d885f1f4139dcadae1
jun <znznun@gmail.com>
2024-12-16 19:44:20 +0900

    update

--- src/hooks/userHooks/useCheckNickname.ts
+++ src/hooks/userHooks/useCheckNickname.ts
@@ -4,22 +4,22 @@ import Swal from "sweetalert2";
 
 const useCheckNickname = () => {
   return useMutation({
-    mutationFn: (nickname: string)=> checkNickname(nickname),
+    mutationFn: (nickname: string) => checkNickname(nickname),
     onError: (error: any) => {
-      if(error.response.status === 409){
+      if (error.response.status === 409) {
         Swal.fire({
-          icon: 'error',
-          title: '사용 불가능한 닉네임',
-          text: '이미 사용중인 닉네임입니다.'
+          icon: "error",
+          title: "사용 불가능한 닉네임",
+          text: "이미 사용중인 닉네임입니다.",
         });
-      } else if(error.response.status === 400){
+      } else if (error.response.status === 400) {
         Swal.fire({
-          icon: 'error',
-          title: '사용 불가능한 닉네임',
-          text: '닉네임은 2~10자의 한글 또는 영문자이어야 합니다.'
+          icon: "error",
+          title: "사용 불가능한 닉네임",
+          text: "닉네임은 2~10자의 한글 또는 영문자이어야 합니다.",
         });
       }
-    }
+    },
   });
 };
 
